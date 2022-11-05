6ff18c0982dba0d643781ae0f867eedcc7ea7e8c
nvhoan11092003 <nvhoan11092003@gmail.com>
2022-11-05 22:29:24 +0700

    trang chu

---
+++ view/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="fonts/BerkshireSwash-Regular.ttf">
+    <link rel="stylesheet" href="fontawesome-free-6.2.0-web/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="restaurant">
+        <div class="header">
+            <div class="grid wide">
+                <div class="header-control">
+                    <img src="image/logo-Sunhomes-BBQ-01.png" alt="" class="logo height-center">
+                    <ul class="nav height-center">
+                        <li><a href="">Trang chủ</a></li>
+                        <li><a href="">Giới thiệu</a></li>
+                        <li><a href="">Đặt bàn</a></li>
+                        <li><a href="">Thực đơn</a></li>
+                        <li><a href="">Tin tức</a></li>
+                        <li><a href="">Liên hệ</a></li>
+                        <li><a href="">đăng nhập</a></li>
+                        <li><a href="" class="hotline">Hotline: 0984966806</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
