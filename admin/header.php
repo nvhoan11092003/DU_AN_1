b5693fed91d4c90b51edd88a17e7ce6782933337
vaph26494 <va66221199@gmail.com>
2022-11-07 07:48:21 +0700

    sanpham 2

---
+++ admin/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="boxcenter">
+    <img class="banner" src="../view/image/oke.jpg">
+        <div class="row mb header admin">
+            <h1 style="text-align:center">ADMIN</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="index.php">Trang Chủ</a></li>
+                <li><a href="index.php?act=adddm">Danh Mục</a></li>
+                <li><a href="index.php?act=addsp">Món Ăn</a></li>
+                <li><a href="index.php?act=dskh">Khách Hàng</a></li>
+            </ul>
+</div>
+<div class="">
