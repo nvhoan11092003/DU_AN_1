8f961501577c59f48ffd2275be7003ecbc466080
vaph26494 <va66221199@gmail.com>
2022-11-24 08:38:47 +0700

    sp_cungloai

--- view/chitiet_sp.php
+++ view/chitiet_sp.php
@@ -23,27 +23,41 @@ $img = $img_path . $img;
         </form>
         <h4 class="product-other-title">Các sản phẩm cùng loại</h2>
             <div class="row">
-                <div class="col l-3">
+                <?php
+                $sp_cungloai = load_sanpham_cungloai($id,$iddm);
+                foreach ($sp_cungloai as $sp) {
+                    extract($sp);
+
+                    echo'
+                    <div class="col l-3">
                     <a href="" style="text-decoration: none;">
-                        <form action="">
+                        <form action="" method="POST" enctype="multipart/form-data">
                             <div class="product-other">
-                                <img src="view/image/4534545345.jpg" alt="" class="other-img">
+                                <img src=upload/'.$img.' alt="" class="other-img">
                             </div>
-                            <h5 class="other-name">Nước lẩu Tomyum ( 2 Lít)-D</h5>
-                            <h6 class="other-price">166,000 đ</h6>
-                            <input type="text" name="id" hidden value="">
-                            <input type="text" name="name" hidden value="">
-                            <input type="text" name="price" hidden value="">
-                            <input type="text" name="img" hidden value="">
-                            <input type="text" name="mota" hidden value="">
+                            <h5 class="other-name">'.$name.'</h5>
+                            <h6 class="other-price">'.$price.'đ</h6>
+                            <input type="text" name="'.$id.'" hidden value="">
+                            <input type="text" name="'.$name.'" hidden value="">
+                            <input type="text" name="'.$price.'" hidden value="">
+                            <input type="text" name="'.$img.'" hidden value="">
+                            <input type="text" name="'.$mota.'" hidden value="">
                             <input type="submit" value="Đặt ngay" class="submit-order">
                         </form>
                     </a>
                 </div>
+                    ';
+
+                }
+                ?>
             </div>
 
-            <h4 class="product-other-title">Có thể bạn thích</h2>
-                <div class="row">
+
+
+
+
+            <!-- <h4 class="product-other-title">Có thể bạn thích</h2> -->
+                <!-- <div class="row">
                     <div class="col l-3">
                         <a href="">
                             <form action="">
@@ -61,6 +75,6 @@ $img = $img_path . $img;
                             </form>
                         </a>
                     </div>
-                </div>
+                </div> -->
     </div>
 </div>
