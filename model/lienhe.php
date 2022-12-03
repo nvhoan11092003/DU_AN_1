5e3abaab1309fb4b3755208470e510f5d9a85f49
nvhoan11092003 <nvhoan11092003@gmail.com>
2022-12-03 08:41:06 +0700

    lienhe

--- model/lienhe.php
+++ model/lienhe.php
@@ -6,11 +6,21 @@ function insert_contact($name,$email,$tel,$purpose,$description,$id)
     pdo_execute($sql);
 }
 
+
 function loadall_contact()
 {
     $sql = "SELECT * FROM contact";
     $listcontact= pdo_query($sql);
     return $listcontact;
+}
+
+function loadpage_contact($start_record , $record_per_page)
+{
+    $sql = "SELECT * FROM contact ORDER BY id DESC  LIMIT $start_record , $record_per_page ";
+    $contact = pdo_query($sql);
+    // hiển thị dữ liệu dưới dạng json
+   
+    echo json_encode($contact);
 }
 
 function delete_contact($id)
